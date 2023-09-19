bce2aba0814929e10fcb988f2c8884790bdb31eb
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2023-09-19 17:29:49 +0300

    my js and styling files

--- drag-drop/src/App.js
+++ drag-drop/src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
 
-function App() {
+function MyDragDropUploader() {
+  const [images, setImages] = useState([]);
+  const [isUserDragging, setIsUserDragging] = useState(false);
+  const filmInputRef = useRef(null);
+
+  function pickFilms() {
+    filmInputRef.current.click();
+  }
+
+  function onFilmPick(event) {
+    const films = event.target.films;
+    if(films.length == 0) return;
+    for (let x = 0; x < Array.length; x++) {
+      if(films[x].type.split('/')[0] !== 'image') continue;
+      if(!images.some((e)=> e.name === films[x].name)) {
+        setImages((prevImages) => [
+          ...prevImages,
+          {
+            name:films[x].name,
+            url: URL.createObjectURL(films[x]),
+          },
+        ]);
+      }
+    }
+  }
+
+  function  removeImage(index) {
+    setImages((prevImages) =>
+      prevImages.filter((_, x) => x !== index)
+    );
+  }
+
+  function onDragOver(event) {
+    event.preventDefault();
+    setIsUserDragging(true);
+    event.dataTransfer.dropEffect = 'copy';
+  }
+
+  function onDragLeave(event) {
+    event.preventDefault();
+    setIsUserDragging(false);
+  }
+
+  function onDrop(event) {
+    event.preventDefault();
+    setIsUserDragging(false);
+    const films = event.dataTransfer.films;
+  }
+
+  const emojiStyle = {
+    display: 'inline',
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="card">
+      <div className='top'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome User! drag N drop your images here <h3 style={emojiStyle}>&#128071;</h3>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className='drag-plot' onDragOver={onDragOver} onDragLeave={onDragLeave} onDrop={onDrop}>
+        {isUserDragging ? (
+          <span className='pick'>Drop image here</span>
+        ):(
+          <>
+            Drag N Drop picture here or {" "}
+          <span className='pick' role='button' onclick={pickFilms}>
+            SearchFolders
+          </span>
+          </>
+        )}
+        <input name='film' type='film' className='film' multiple ref={filmInputRef} onChange={onFilmPick}></input>
+      </div>
+      <div className='container'>
+        {images.map((images,index) =>(
+          <div className='image' key={index}>
+            <span className='remove' onClick={() => removeImage(index)}>&times;</span>
+            <img src={images.url} alt={images.name}/>
+          </div>
+        ))}   
+      </div>
+      <button type='button'>
+        Upload
+      </button>
     </div>
   );
 }
 
-export default App;
+export default MyDragDropUploader;
